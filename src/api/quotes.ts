9df201fc394ab433f9e97f7190e5aac716ffe8c1
Maxim Kosterov <maxim.kosterov@gmail.com>
2022-10-07 20:14:22 +0300

    Quote element

--- src/api/quotes.ts
+++ src/api/quotes.ts
@@ -7,7 +7,7 @@ export const getQuotes = async (): Promise<Quote[]> => {
   return [
     { author: "Me", contents: "Hah" },
     {
-      author: "Me",
+      author: "Me KsenyK",
       contents:
         "This is a looong quote, " +
         "lorem ipsum dolor net. Hi dear me omg hi haha.",
